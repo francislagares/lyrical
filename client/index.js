d101cc51acce644b1d619d51557ca11091d3a15e
Francis Lagares <francis.lagares@gmail.com>
2020-08-19 14:12:04 +0200

    feat: implement react routes

--- client/index.js
+++ client/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-} from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloProvider } from 'react-apollo';
+
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+
+import './style/style.css';
 
 const client = new ApolloClient({
   link: new HttpLink(),
@@ -15,7 +17,24 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <Router>
+        <nav>
+          <div className='container nav-wrapper'>
+            <ul id='nav-mobile' className='left hide-on-med-and-down'>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/songs/new'>Create a Song</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={SongList} />
+          <Route path='/songs/new' component={SongCreate} />
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
 };
