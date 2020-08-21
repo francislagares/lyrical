3db98e007e950a7fe5e7e067760725b7440ac9db
Francis Lagares <francis.lagares@gmail.com>
2020-08-21 12:19:10 +0200

    feat(mutations): implement mutation to add lyric

--- client/queries/index.js
+++ client/queries/index.js
@@ -1,10 +1,23 @@
 import { gql } from '@apollo/client';
 
 export const querySongList = gql`
-  {
+  query SongList {
     songs {
       id
       title
+    }
+  }
+`;
+
+export const querySong = gql`
+  query SongQuery($id: ID!) {
+    song(id: $id) {
+      id
+      title
+      lyrics {
+        id
+        content
+      }
     }
   }
 `;
@@ -23,6 +36,18 @@ export const mutationDeleteSong = gql`
     deleteSong(id: $id) {
       id
       title
+    }
+  }
+`;
+
+export const mutationAddLyric = gql`
+  mutation addLyricToSong($content: String!, $songId: ID!) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      title
+      lyrics {
+        content
+      }
     }
   }
 `;
